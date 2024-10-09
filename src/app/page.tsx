55861069e72f9865d6bd29f157502e9a00a11aae
Harshini Raju <59364581+HarshiniR4@users.noreply.github.com>
2024-10-09 16:26:18 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import MainPage from './components/MainPage';
 import Blogs from './components/Blogs';
 import Experience from './components/Experience';
 import HireMe from './components/HireMe';
-import Navbar from './components/Navbar';
 import Sidebar from './components/SideBar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
