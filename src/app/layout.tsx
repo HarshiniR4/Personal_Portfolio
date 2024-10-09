93032630c8154c6b40e6ea7c9efd965b63b9c0ec
HarshiniR4 <harshiniraju4@gmail.com>
2024-10-09 19:39:34 -0400

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Sidebar from './components/SideBar';
 import Header from './components/Header';
 import './globals.css';
 import { ReactNode, useState } from 'react';
+import { Analytics } from "@vercel/analytics/react"
 
 interface LayoutProps {
   children: ReactNode;
@@ -24,6 +25,7 @@ export default function Layout({ children }: LayoutProps) {
         <div className="main-content">
           {children}
         </div>
+        <Analytics />
       </body>
     </html>
   );
